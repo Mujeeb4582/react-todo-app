4fa480ee0c152491d01be80fa32e48f75a85c2c9
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-06 15:17:57 +0500

    Add inline styling to the TodoItem

--- src/componenets/TodoItem.js
+++ src/componenets/TodoItem.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './TodoItem.module.scss';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TodoItem extends React.Component {
   render() {
     const { todo, handleChangeProps, deleteTodoProps } = this.props;
+
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: '#090909',
+      opacity: 0.4,
+      textDecoration: 'line-through',
+    };
+
     return (
-      <li>
-        <input type="checkbox" checked={todo.completed} onChange={() => handleChangeProps(todo.id)} />
+      <li className={styles.item}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={todo.completed}
+          onChange={() => handleChangeProps(todo.id)}
+        />
         {' '}
-        <button type="button" onClick={() => deleteTodoProps(todo.id)}>Delete</button>
-        {todo.title}
+        <button type="button" onClick={() => deleteTodoProps(todo.id)}>
+          Delete
+        </button>
+
+        <span style={todo.completed ? completedStyle : null}>
+          {todo.title}
+        </span>
+
       </li>
     );
   }
 }
 
 TodoItem.propTypes = {
-  todo: (PropTypes.shape({
+  todo: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     completed: PropTypes.bool.isRequired,
-  })).isRequired,
+  }).isRequired,
   handleChangeProps: PropTypes.func.isRequired,
   deleteTodoProps: PropTypes.func.isRequired,
 };
