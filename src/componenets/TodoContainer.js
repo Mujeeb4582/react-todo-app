98fcbc7c91770b57daec8db318b80263626c9050
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-06 22:43:27 +0500

    Add the local storage feature

--- src/componenets/TodoContainer.js
+++ src/componenets/TodoContainer.js
@@ -8,23 +8,7 @@ class TodoContainer extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: [
-        {
-          id: uuidv4(),
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: uuidv4(),
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          title: 'Deploy to live server',
-          completed: true,
-        },
-      ],
+      todos: [],
     };
 
     this.handleChange = (id) => {
@@ -74,6 +58,24 @@ class TodoContainer extends React.Component {
         }),
       });
     };
+  }
+
+  componentDidMount() {
+    const temp = localStorage.getItem('todos');
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { todos } = this.state;
+    if (prevState.todos !== todos) {
+      const temp = JSON.stringify(todos);
+      localStorage.setItem('todos', temp);
+    }
   }
 
   render() {
