f7b4579a81c2a8837e09dae1f9c2a9e63f136274
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-06 14:29:11 +0500

    Add the feature to handle the events

--- src/componenets/TodoItem.js
+++ src/componenets/TodoItem.js
@@ -4,11 +4,12 @@ import PropTypes from 'prop-types';
 // eslint-disable-next-line react/prefer-stateless-function
 class TodoItem extends React.Component {
   render() {
-    const { todo, handleChangeProps } = this.props;
+    const { todo, handleChangeProps, deleteTodoProps } = this.props;
     return (
       <li>
         <input type="checkbox" checked={todo.completed} onChange={() => handleChangeProps(todo.id)} />
         {' '}
+        <button type="button" onClick={() => deleteTodoProps(todo.id)}>Delete</button>
         {todo.title}
       </li>
     );
@@ -17,11 +18,12 @@ class TodoItem extends React.Component {
 
 TodoItem.propTypes = {
   todo: (PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     completed: PropTypes.bool.isRequired,
   })).isRequired,
   handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
