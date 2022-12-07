be6892359d683e5e5463f5c3ddfb79022237b976
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-07 15:10:45 +0500

    Add the index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// component file
+import TodoContainer from './functionBased/components/TodoContainer';
+
+// stylesheet
+import './functionBased/index.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
